d38875b50a86fd173cf1cd60f378ea115ad809bc
Sovarim <abdul.taramov2001@gmail.com>
2021-07-22 13:59:55 +0300

    модель пользователя

--- server.js
+++ server.js
@@ -1,6 +1,26 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+require("dotenv").config();
+
 const { PORT } = require("./config");
 const app = express();
+
+mongoose.connect(process.env.mongoURI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+  useCreateIndex: true,
+});
+
+app.use(cors());
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
   res.json({ id: 1, text: "hello" });
